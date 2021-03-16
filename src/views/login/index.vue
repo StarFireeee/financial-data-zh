f158412d8ea3d5e7275def18fe1e45d616e2aa6b
liuhj <liuhj@hst.com>
2021-03-16 15:17:55 +0800

    添加登录页

---
+++ src/views/login/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ base }}
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      base: 'Hello,World'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+div{
+  height: 300px;
+}
+</style>
