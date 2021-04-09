aff530e24d967da86e5b0dc1fb721069c4807004
StarFireeee <wind296678436@outlook.com>
2021-04-09 18:15:16 +0800

    sidebar

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="sidebar-logo-container">
-    <router-link to='/'>
+    <router-link to='/' class="sidebarRouter">
       <img :src="src" class="sidebarlogo">
+      <div class="sidebarTitle">{{title}}</div>
     </router-link>
   </div>
 </template>
@@ -12,12 +13,30 @@ export default {
   data() {
     return {
       title: '星火系统',
-      src: require('@/assets/logo-02.png')
+      src: require('@/assets/logo-01.png')
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.sidebar-logo-container{
+  height: 64px;
+  background: #108ee9;
+  .sidebarRouter{
+    width: 100%;
+    display: flex;
+    img{
+      height: 53px;
+      margin: 5px;
+    }
+    .sidebarTitle{
+      color: rgb(255, 195, 83);
+      font-size: 20px;
+      line-height: 64px;
+      text-align: center;
+      width: 100%;
+    }
+  }
+}
 </style>
