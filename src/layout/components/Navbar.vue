045a1b91716b9b801020c80513c307d2f1abc5e0
StarFireeee <wind296678436@outlook.com>
2021-04-26 18:40:17 +0800

    test

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -6,7 +6,7 @@
     <a-breadcrumb>
       <a-breadcrumb-item>Home</a-breadcrumb-item>
       <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
-      <a-breadcrumb-item><a href="">Application List</a></a-breadcrumb-item>
+      <a-breadcrumb-item><a href="">List</a></a-breadcrumb-item>
       <a-breadcrumb-item>An Application</a-breadcrumb-item>
     </a-breadcrumb>
   </div>
